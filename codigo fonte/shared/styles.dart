1d49c4498837c0ceb052d85dd11f6b8343a0afbd
Ticiane Cabral <114961257+tici10@users.noreply.github.com>
2025-10-16 18:38:35 -0300

    Add files via upload

---
+++ codigo fonte/shared/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:design_system/shared/colors.dart';
+
+const TextStyle nomeproduto = TextStyle(
+  fontSize: 16.0,
+  fontWeight: FontWeight.bold,
+  color: Color(0x000000),
+);
+
+const TextStyle precoproduto = TextStyle(
+  fontSize: 18.0,
+  fontWeight: FontWeight.w800,
+  color: Color(0xAE8549),  
+);
