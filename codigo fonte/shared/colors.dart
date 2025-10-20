0dc9d80a07a60dfd1d32804b9fb3158cccb649ec
Ticiane Cabral <114961257+tici10@users.noreply.github.com>
2025-10-20 00:22:53 -0300

    Add files via upload

---
+++ codigo fonte/shared/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryBaseColor = Color(0x3C2605);
+const Color secondaryBaseColor = Color(0xAE8549);
+const Color tertiaryBaseColor = Color(0xD2CCC5);
+
