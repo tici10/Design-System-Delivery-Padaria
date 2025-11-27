deda4e898cbaad847161c9135011ea20d7095673
Ticiane Cabral <114961257+tici10@users.noreply.github.com>
2025-11-27 17:36:23 -0300

    Refactor CardViewModel to include size and style

--- codigo fonte/Componentes/Card/view_model_card.dart
+++ codigo fonte/Componentes/Card/view_model_card.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
+enum CardStyle{
+  unique,
+}
+
+enum CardSize{
+  medium,
+  large,
+}
+
 class CardViewModel{
-  final String imageUrl;
+  final CardSize size;
+  final CardStyle style;
+  final Image image;
   final String nome;
   final String preco;
 
   CardViewModel({
-    required this.imageUrl,
+    required this.size,
+    required this.style,
+    required this.image,
     required this.nome,
     required this.preco,
   });
